dd6333b0268c81132eecaffb77c0ab41d95756fe
Isabel <isabel.oliveira@mosten.com>
2026-03-02 19:18:33 -0300

    fix: adiciona 'catch' no middleware para capturar DomainExceptions

--- src/CompraProgramada.Api/Middlewares/ValidationExceptionMiddleware.cs
+++ src/CompraProgramada.Api/Middlewares/ValidationExceptionMiddleware.cs
@@ -1,5 +1,7 @@
-﻿using System.Net;
+﻿using CompraProgramada.Domain.Exceptions;
+using System.Net;
 using System.Text.Json;
+using ValidationException = CompraProgramada.Application.Common.Exceptions.ValidationException;
 
 namespace CompraProgramada.Api.Middlewares
 {
@@ -18,7 +20,7 @@ namespace CompraProgramada.Api.Middlewares
             {
                 await _next(httpContext);
             }
-            catch (CompraProgramada.Application.Common.Exceptions.ValidationException ex)
+            catch (ValidationException ex)
             {
                 httpContext.Response.ContentType = "application/json";
                 httpContext.Response.StatusCode = (int)HttpStatusCode.BadRequest;
@@ -26,6 +28,30 @@ namespace CompraProgramada.Api.Middlewares
                 var result = JsonSerializer.Serialize(new
                 {
                     errors = ex.Errors
+                });
+
+                await httpContext.Response.WriteAsync(result);
+            }
+            catch (DomainException ex)
+            {
+                httpContext.Response.ContentType = "application/json";
+                httpContext.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+
+                var result = JsonSerializer.Serialize(new
+                {
+                    error = ex.Message
+                });
+
+                await httpContext.Response.WriteAsync(result);
+            }
+            catch (Exception)
+            {
+                httpContext.Response.ContentType = "application/json";
+                httpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+                var result = JsonSerializer.Serialize(new
+                {
+                    error = "Erro interno do servidor."
                 });
 
                 await httpContext.Response.WriteAsync(result);
